f2d5b30e7857f251c45d56788ded7ce9567c154a
alancosiomx <c.alancosio@gmail.com>
2025-06-11 15:14:03 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,14 @@
 <?php
-header("Location: /index.php"); // o al login o donde quieras
-exit;
+header("Refresh: 5; URL=/index.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Página no encontrada</title>
+</head>
+<body style="font-family: sans-serif; text-align: center; padding-top: 50px;">
+  <h1>404 - Página no encontrada</h1>
+  <p>Serás redirigido en unos segundos...</p>
+</body>
+</html>
