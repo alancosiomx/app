1fc7c2cff4e880110da8bb4bc788683bb40f3d5a
alancosiomx <c.alancosio@gmail.com>
2025-06-11 15:41:08 -0500

    Create resultado_bbva.php

---
+++ admin/resultado_bbva.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/init.php';
+
+$usuario = $_SESSION['usuario_nombre'] ?? 'Administrador';
+$contenido = __DIR__ . '/contenido_resultado_bbva.php';
+
+require_once __DIR__ . '/layout.php';
