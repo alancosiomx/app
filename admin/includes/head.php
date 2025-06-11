584ce5bfcc79a1174f360b6eca3aa51182a88808
alancosiomx <c.alancosio@gmail.com>
2025-06-11 11:50:50 -0500

    Update head.php

--- admin/includes/head.php
+++ admin/includes/head.php
@@ -5,7 +5,7 @@
   <title>Panel OMNIPOS</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-..." crossorigin="anonymous">
   <style>
     body {
       margin: 0;
