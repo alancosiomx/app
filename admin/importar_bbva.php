7aaed78a6a925dca515283f9f512dd00e55d5bfb
alancosiomx <c.alancosio@gmail.com>
2025-06-11 15:39:15 -0500

    Update importar_bbva.php

--- admin/importar_bbva.php
+++ admin/importar_bbva.php
@@ -3,8 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/init.php'; //
-require HEAD;
-require MENU;
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
