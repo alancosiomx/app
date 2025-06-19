91d6b0d62d11d5258bbf5182530726aa285c1807
alancosiomx <c.alancosio@gmail.com>
2025-06-19 12:39:08 -0500

    Update config.php

--- front_whatsapp/config.php
+++ front_whatsapp/config.php
@@ -1,1 +1,7 @@
-d
+<?php
+// Configuración de Twilio WhatsApp
+return [
+    'sid'   => 'AC607efa330bae90ab6be43bc12d58622e',
+    'token' => 'b47e5e5365d06665301c0b7b222b81b2',
+    'from'  => 'whatsapp:+14155238886', // Sandbox de Twilio
+];
