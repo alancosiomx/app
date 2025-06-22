fc4ba6ac4d101f80ddb8ed4feaabaaa7d1ba4ba4
alancosiomx <c.alancosio@gmail.com>
2025-06-22 18:01:04 -0500

    Update constants.php

--- admin/finanzas/constants.php
+++ admin/finanzas/constants.php
@@ -1,9 +1,7 @@
-<?php
-// /constants.php
-
 define('TABS_FINANZAS', [
   'cobros'    => '💳 Reporte de Cobros',
   'pagos'     => '📦 Pagos por Técnico',
   'viaticos'  => '🧾 Viáticos',
   'historial' => '📂 Historial de Pagos',
+  'precios'   => '⚙️ Precios por Técnico',
 ]);
