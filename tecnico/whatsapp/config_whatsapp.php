05e0638ff69fc66f8148d99b4c52119c1a45d507
alancosiomx <c.alancosio@gmail.com>
2025-06-27 07:13:35 -0500

    Create config_whatsapp.php

---
+++ tecnico/whatsapp/config_whatsapp.php
@@ -0,0 +1,5 @@
+// /app/tecnico/whatsapp/config_whatsapp.php
+<?php
+// Parámetros fijos que puedes necesitar después:
+define('TOKEN_MANYCHAT', ''); // si usas autenticación opcional
+define('NUMEROS_AUTORIZADOS', ['+5215555555555', '+5211234567890']);
