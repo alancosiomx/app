ead51db5efbc9c00964c8500094ba4d6deb5a7cb
alancosiomx <c.alancosio@gmail.com>
2025-07-05 15:16:21 -0500

    Update contenido_cobros.php

--- admin/finanzas/contenido_cobros.php
+++ admin/finanzas/contenido_cobros.php
@@ -8,7 +8,7 @@ $desde = $_GET['desde'] ?? '';
 $hasta = $_GET['hasta'] ?? '';
 $tecnico = $_GET['tecnico'] ?? '';
 
-$condiciones = ["actual_status = 'Histórico'", "resultado IN ('Exito', 'Rechazo')"];
+$condiciones = ["estatus = 'Histórico'", "resultado IN ('Exito', 'Rechazo')"];
 $parametros = [];
 
 if ($desde && $hasta) {
