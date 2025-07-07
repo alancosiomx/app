28160432ed39a4caddf84bf00ac996eb3200fd92
alancosiomx <c.alancosio@gmail.com>
2025-07-06 21:18:39 -0500

    Update constants.php

--- admin/sims/constants.php
+++ admin/sims/constants.php
@@ -1,6 +1,8 @@
 <?php
+// app/admin/sims/constants.php
 $tabs_sims = [
     'inventario' => '📦 Inventario',
     'asignar'    => '👤 Asignar SIMs',
+    'alta'       => '🆕 Alta de SIMs',
     'logs'       => '📜 Movimientos'
 ];
