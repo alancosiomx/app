aebbbf4c6ee97314550dea0a6f7012524d0b19dc
alancosiomx <c.alancosio@gmail.com>
2025-07-07 10:04:39 -0500

    Update procesar_factura.php

--- admin/factura/procesar_factura.php
+++ admin/factura/procesar_factura.php
@@ -1,7 +1,7 @@
-
 <?php
-require_once __DIR__ . '/../../config.php';
 if (session_status() === PHP_SESSION_NONE) session_start();
+require_once __DIR__ . '/../../config.php';
+
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: index.php?vista=nueva");
