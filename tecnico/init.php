083c6915df2b98ad161b68ef4d84cc17a020206b
alancosiomx <c.alancosio@gmail.com>
2025-07-16 21:19:48 -0500

    Create init.php

---
+++ tecnico/init.php
@@ -0,0 +1,14 @@
+<?php
+// app/tecnico/init.php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../config.php';
+session_start();
+
+if (!isset($_SESSION['usuario_username'])) {
+    header("Location: /login.php");
+    exit;
+}
