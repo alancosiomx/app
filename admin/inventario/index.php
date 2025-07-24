4593c9fa8a0375aefc4755fc2f31ff893d9cbd21
alancosiomx <c.alancosio@gmail.com>
2025-07-24 13:58:03 -0500

    Update index.php

--- admin/inventario/index.php
+++ admin/inventario/index.php
@@ -1,10 +1,10 @@
+<?php
 require_once __DIR__ . '/../init.php';
 
 if (!tienePermiso('ver_inventario')) {
     die('⛔ Acceso denegado');
 }
 
-$vista = $_GET['vista'] ?? 'index'; // valores: index, asignar, editar, etc.
-$contenido = __DIR__ . "/contenido_{$vista}.php";
+$contenido = __DIR__ . '/contenido_index.php';
 
 require_once __DIR__ . '/../layout.php';
